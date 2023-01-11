80ed8d79b9f83d9e5b9a104f0dacd19a1c94ea63
OKHAEHO <https://github.com/OKHAEHO/javascript.git>
2023-01-11 10:30:08 +0900

    03.day

--- 03.day/01.js
+++ 03.day/01.js
@@ -4,7 +4,7 @@ var person = Object();
 
 //만들어진 빈 오브젝트 안에 속성과 메서드 설정 
 person.firstName = "재호";
-person.lastName = "옥";
+person.lastName = "억";
 person.age = 15;
 person.getFullName = function () {
   return this.firstName + " " + this.lastName;
--- 03.day/02.js
+++ 03.day/02.js
@@ -1,4 +1,5 @@
 let num1 = 50;
 const num2 = 123456.1234;
 console.log(num2.toFixed(2));
-console.log(num2.toPrecision(5));
+console.log(num2.toPrecision(1));
+
